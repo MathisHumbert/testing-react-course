5f20cd2bdb7647553b9a1672a1eca43f1345fb6a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-07-20 16:22:31 +0200

    simulating server response with msw

---
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Row } from 'react-bootstrap';
+
+import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBAnner';
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((err) => setError(true));
+  }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
